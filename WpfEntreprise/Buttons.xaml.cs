3671888d3e593662d67d0149a882cb100fd7c56b
Assam Toumi <assamtomi@outlook.com>
2020-07-03 20:55:56 +0100

    done!

---
+++ WpfEntreprise/Buttons.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfEntreprise
+{
+    /// <summary>
+    /// Interaction logic for Buttons.xaml
+    /// </summary>
+    public partial class Buttons : Page
+    {
+        public Buttons()
+        {
+            InitializeComponent();
+        }
+
+        private void OrdinaryButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Ordinary Button");
+        }
+
+        private void RepeatButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Repeat Button");
+        }
+        private void ToggleButton_OnClick(object sender, RoutedEventArgs e) 
+        {
+            Debug.WriteLine("Toggle Button");
+        }
+        #region Checked-Uncheched-Indeterminate
+        private void ToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+            var toggleButton = (ToggleButton) sender;
+            Debug.WriteLine("{0} is checked. Now IsChecked={1}", toggleButton.Name, toggleButton.IsChecked);
+        }
+
+        private void ToggleButton_UnChecked(object sender, RoutedEventArgs e)
+        {
+            var toggleButton = (ToggleButton)sender;
+            Debug.WriteLine("{0} is checked. Now IsChecked={1}", toggleButton.Name, toggleButton.IsChecked);
+
+        }
+        private void ToggleButton_Indeterminate(object sender, RoutedEventArgs e)
+        {
+            var toggleButton = (ToggleButton)sender;
+            Debug.WriteLine("{0} is checked. Now IsChecked={1}", toggleButton.Name, toggleButton.IsChecked);
+
+        }
+        #endregion
+    }
+}
---
+++ WpfEntreprise/MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfEntreprise
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page1();
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page2();
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page1();
+        }
+
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page1();
+        }
+
+        private void Button5_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page1();
+        }
+
+        private void Button6_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page1();
+        }
+
+        private void Button7_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page1();
+        }
+
+        private void Button8_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page1();
+        }
+
+        private void Button9_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page1();
+        }
+
+        private void Button10_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page1();
+        }
+
+        private void Button11_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page1();
+        }
+
+        private void Button12_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page1();
+        }
+
+        private void Button13_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page1();
+        }
+
+        private void Button14_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page1();
+        }
+
+        private void Button15_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page1();
+        }
+
+        private void Button16_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page1();
+        }
+        private void Button17_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page1();
+        }
+
+        private void Button18_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page1();
+        }
+        private void Button19_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page1();
+        }
+    }
+}
